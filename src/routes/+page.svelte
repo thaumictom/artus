63690ca7a1d49a3cb7422eacedeff5f6bf139e8e
thaumictom <tom@thaumictom.de>
2026-04-21 20:39:50 +0200

    Remove shadcn

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,3 @@
 <main class="p-4">
-	<h1 class="font-semibold text-xl">Artus</h1>
-	<p class="mt-2">
-		Open <a class="underline" href="/artus">/artus</a>
-		or
-		<a class="underline" href="/overlay">/overlay</a>
-		.
-	</p>
+	<h1 class="font-semibold text-xl">artus</h1>
 </main>
